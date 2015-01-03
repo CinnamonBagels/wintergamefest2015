8117360466f1bb19329643091126127852d85110
cinnamonbagels <samuelsko@gmail.com>
2015-01-02 17:37:32 -0800

    layout complete

---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function() {
+	$('#mario-foreground').scrollingParallax();
+	$('#zelda-foreground').scrollingParallax();
+	$.scrollingParallax('./img/2_mario/mario_2.png');
+	$.scrollingParallax('./img/2_mario/mario_3.png');
+	$.scrollingParallax('./img/2_mario/mario_4.png');
+	$.scrollingParallax('./img/2_mario/mario_5.png');
+})();
